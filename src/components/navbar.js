b690d42355a21c23e82318494463c6c0879f135f
Anis <anisbendib@gmail.com>
2022-12-13 19:21:53 +0100

    Added navigation list

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,13 @@ export default function Navbar(){
         <div className="navbar">
             <img src={require("../assets/logo.png")} id="logo"/>
             <h3 id="logo-title">Airbnb</h3>
-            <br/>
+           
+                <ul className="nav-list">
+                    <a href="/home">Home</a>
+                    <a href="/home">Contact</a>
+                    <a href="/home">About</a>
+                </ul>
+            
         </div>
     )
 
